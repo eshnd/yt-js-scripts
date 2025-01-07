ddeacb6c6494be9b6a2d8fcda784ce0dfc7c16a0
eshaan <eshdesh@null.net>
2025-01-06 20:39:51 -0600

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,2 +1,3 @@
 document.querySelectorAll('.rich-section-content').forEach(div => div.remove());
 document.querySelectorAll('.pivot-shorts').forEach(div => div.remove()); 
+document.querySelectorAll('.reel-shelf-items').forEach(div => div.remove()); 
