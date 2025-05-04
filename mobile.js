3d28fe49f610b03e323866129b2722946716b5db
eshaan <eshaan2031@icloud.com>
2025-05-04 14:01:16 -0500

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,4 +1,7 @@
-document.querySelectorAll('.rich-section-content').forEach(div => div.remove());
-document.querySelectorAll('.pivot-shorts').forEach(div => div.remove()); 
-document.querySelectorAll('.reel-shelf-items').forEach(div => div.remove()); 
-document.querySelectorAll('.mobile-topbar-header-endpoint').forEach(div => div.remove()); 
+setInterval(() => {
+  document.querySelectorAll('.rich-section-content').forEach(div => div.remove());
+  document.querySelectorAll('.pivot-shorts').forEach(div => div.remove());
+  document.querySelectorAll('.reel-shelf-items').forEach(div => div.remove());
+  document.querySelectorAll('.mobile-topbar-header-endpoint').forEach(div => div.remove());
+}, 100);
+
