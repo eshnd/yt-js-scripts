8c120bf377007bd404ebc6ff5f02d7dc0b62a3a9
eshaan <eshaan2031@icloud.com>
2025-05-04 14:14:09 -0500

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -2,6 +2,5 @@ setInterval(() => {
   document.querySelectorAll('.rich-section-content').forEach(div => div.remove());
   document.querySelectorAll('.pivot-shorts').forEach(div => div.remove());
   document.querySelectorAll('.reel-shelf-items').forEach(div => div.remove());
-  document.querySelectorAll('.mobile-topbar-header-endpoint').forEach(div => div.remove());
 }, 100);
 
